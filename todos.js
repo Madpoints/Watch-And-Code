bfb00425b1bdac6fa70171ed58aba2c050f68e95
John Furtado <jf8846@gmail.com>
2017-07-13 23:14:00 +0000

    version2

--- todos.js
+++ todos.js
@@ -1,19 +1,6 @@
 // creat list(array) of TODOS
 var todos = ["item1", "item2", "item3"];
 
-console.log("Display TODOS:", todos);
-
-// add TODOS
-todos.push("item4");
-
-console.log("Add TODOS:", todos);
-
-// change TODOS
-todos[0] = "item updated";
-
-console.log("Update TODOS:", todos);
-
-// remove TODOS
-todos.splice(0, 1);
-
-console.log("Remove TODOS:", todos);
+function displatTODOS() {
+    console.log("My TODOS:", todos);
+}
